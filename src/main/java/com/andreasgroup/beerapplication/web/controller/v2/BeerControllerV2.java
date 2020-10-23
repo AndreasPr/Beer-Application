f601d7fa33b97467801d6ee9d9ce195a87557271
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2020-10-23 11:47:28 -0400

    Addition of some Lombok Annotations in BeerController of version 2.

--- src/main/java/com/andreasgroup/beerapplication/web/controller/v2/BeerControllerV2.java
+++ src/main/java/com/andreasgroup/beerapplication/web/controller/v2/BeerControllerV2.java
@@ -2,32 +2,35 @@ package com.andreasgroup.beerapplication.web.controller.v2;
 
 import com.andreasgroup.beerapplication.services.v2.BeerServiceV2;
 import com.andreasgroup.beerapplication.web.model.v2.BeerDtoV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 /**
  * Created on 20/Oct/2020 to beer-application
  */
+@Slf4j
 @Validated
+@RequiredArgsConstructor
 @RequestMapping("/api/v2/beer")
 @RestController
 public class BeerControllerV2 {
 
     private final BeerServiceV2 beerServiceV2;
 
-    public BeerControllerV2(BeerServiceV2 beerServiceV2) {
-        this.beerServiceV2 = beerServiceV2;
-    }
+    //The constructor was replaced by @RequiredArgsConstructor of Lombok
+//    public BeerControllerV2(BeerServiceV2 beerServiceV2) {
+//        this.beerServiceV2 = beerServiceV2;
+//    }
 
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDtoV2> getBeer(@NotNull @PathVariable("beerId") UUID beerId){
@@ -36,9 +39,12 @@ public class BeerControllerV2 {
 
     @PostMapping
     public ResponseEntity handlePost(@Valid @NotNull @RequestBody BeerDtoV2 beerDto){
-        BeerDtoV2 savedDto = beerServiceV2.saveNewBeer(beerDto);
 
-        HttpHeaders headers = new HttpHeaders();
+        log.debug("In post method...");
+
+        val savedDto = beerServiceV2.saveNewBeer(beerDto);
+
+        val headers = new HttpHeaders();
         headers.add("Location", "/api/v1/beer" + savedDto.getId().toString());
 
         return new ResponseEntity(headers, HttpStatus.CREATED);
